c206887f421b06395cc78521526455a372562ae5
LinaMRuizG <lina.ruiz2@udea.edu.co>
2023-02-14 18:59:10 -0500

    solving conection issues

--- execution.py
+++ execution.py
@@ -9,5 +9,6 @@ if __name__ == "__main__":
     web = 'https://minciencias.gov.co/convocatorias/todas'
     main_web ='https://minciencias.gov.co'
     conv = mc.mincienciasConvoc(web, main_web)
-    conv.get_table()
-    conv.table
+    #conv.run() is the unique method to called from here
+    print(conv.get_table())
+    #conv.save()
