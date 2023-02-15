e56d8813f741cb9fef9e29b0050169bb7c8a2fc6
LinaMRuizG <lina.ruiz2@udea.edu.co>
2023-02-14 19:19:17 -0500

    main some little changes -not important

--- execution.py
+++ execution.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     main_web ='https://minciencias.gov.co'
     conv = mc.mincienciasConvoc(web, main_web)
     #conv.run() is the unique method to called from here
-    print(conv.get_table())
-    #conv.save()
+    (conv.get_table()
+    conv.save()
