a223e0a3debd5d732ce805ce772a88121692b6d6
LinaMRuizG <lina.ruiz2@udea.edu.co>
2023-02-17 09:25:30 -0500

    feat/mani.. last functions are made

--- execution.py
+++ execution.py
@@ -9,6 +9,19 @@ if __name__ == "__main__":
     web = 'https://minciencias.gov.co/convocatorias/todas'
     main_web ='https://minciencias.gov.co'
     conv = mc.mincienciasConvoc(web, main_web)
-    #conv.run() is the unique method to called from here
-    conv.get_table()
-    conv.save()
+    conv.run() 
+    #conv.get_table()
+    #conv.save()
+    #conv.comparing()
+    #conv.emailing()
+    #print(conv.newones)
+
+    # delete and uncoment the last 2 lines after test the function to read and compare tables
+    #setting -I JUST PUT THIS TO SAVE THE TEST FILES TO THE NEXT FUNCTION
+    #from datetime import date, timedelta
+    #first_table = conv.get_table()
+    #uno = first_table[:20].reset_index(drop = True) # this is the most new table because have the first most recent convocatorias
+    #other7 = first_table[2:].reset_index(drop = True)
+    #other14 = first_table[5:].reset_index(drop = True)
+    #other7.to_pickle(f"df_{ date.today() - timedelta(days=7)}")
+    #other14.to_pickle(f"df_{ date.today() - timedelta(days=14)}")
--- minciencias_convoc.py
+++ minciencias_convoc.py
@@ -8,6 +8,10 @@ import ssl # intern conectio secure - ass a layer of security
 import smtplib
 from email.mime.text import MIMEText
 import lxml
+from urllib3.exceptions import InsecureRequestWarning
+from urllib3 import disable_warnings
+
+disable_warnings(InsecureRequestWarning)
 
 class mincienciasConvoc:
 
@@ -15,12 +19,13 @@ class mincienciasConvoc:
     self.__web = web
     self.__mainWeb = mainWeb
     self.__n = nPages if nPages else 5 # the n_pages is optional and the default is 5
+    self.newones = pd.DataFrame()
   
   def __get_links(self, n=None):
 
     """it gives me the links of the n_pages"""
     
-    resp = requests.get(self.__web) # reading the web
+    resp = requests.get(self.__web, verify =False) # reading the web
     lastPage = bsp(resp.text, features="html.parser").find("li", "pager-last last")# looking in the soup the lastPage
     http = lastPage.findChild("a")['href']# looking more into lastPage
     splitedHttp = http.split('page=')
@@ -43,9 +48,84 @@ class mincienciasConvoc:
   def save(self):
 
     """it saves the table"""
-
-    #print("sainving...")
+    
     self.table.to_pickle(f"df_{date.today()}")
+  
+
+
+  def comparing(self, frequency = None):
+
+    """it review if there is previous files-df to compare with the actual df.
+    Then review if there are differences between them: when differences gives a df if not 0 """
+
+    last = frequency if frequency else 7
+    other = pd.DataFrame()
+
+    try:
+      other = pd.read_pickle(f"df_{date.today() - timedelta(days=last)}")
+    except:
+      try:
+        other = pd.read_pickle(f"df_{date.today() - timedelta(days=last*2)}")
+      except:
+        print(f"there are not files for the last 2 runs")
+
+    if len(other) != 0 :
+      merging = self.table.merge(other, how = 'outer', indicator=True)
+      self.newones = merging.loc[merging['_merge']=='left_only'].reset_index(drop = True)
+      self.newones.drop('_merge', inplace = True, axis = 1)
+      #if self.newones.shape[0] != 0:
+      # return self.newones
+      #else:
+      # self.newones = 0
+
+  def __emailing(self, mails = None):
+
+    """it sends an mail if there is new 'convocatorias' 
+    This function is ONLY use if  newones != 0. Thats the reason why it is 
+    private method"""
+
+    email_sender = 'fisicaminciencias@gmail.com'
+    email_password = 'wbauihzgjjviktqb'
+    email_receiver = ['lina.ruiz2@udea.edu.co','anderson.ruales@udea.edu.co']#,'josed.ruiz@udea.edu.co']
+
+    html = f""" <html><head></head><body>
+    <p>Estas son las nuevas convocatorias de MINCIENCIAS:<p>  
+    
+    {self.newones.to_html()}
+    
+    <p> Revisa 'https://minciencias.gov.co/convocatorias/todas' para información sobre las convocatorias <br>
+        Revisa  https://github.com/LinaMRuizG/minciencias_convocatorias para información sobre este proyecto <p> 
+    Good luck!<br>
+    be happy!<p> 
+
+    </body></html>"""
+    body = MIMEText(html, 'html')
+    subject = 'Nuevas convocatorias MINCIENCIAS'
+
+    em=EmailMessage()
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em['Subject'] = subject
+    em.set_content(body)
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com',465, context = context) as smtp:
+      smtp.login(email_sender,email_password)
+      smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+  def run(self):
+    
+    """it runs the methods"""
+    
+    self.get_table()
+    self.save()
+    self.comparing()
+    #print(self.newones.shape[0])
+    #print("No hay nuevas convocatorias") if self.newones.shape[0] == 0 else self.__emailing()
+
+    
+
+
 
 
   
