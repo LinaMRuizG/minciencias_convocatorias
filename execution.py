1912d91416b31c051e4d3ea7bfffa4e605203954
LinaMRuizG <65182910+LinaMRuizG@users.noreply.github.com>
2023-02-23 12:48:04 -0500

    Update execution.py

--- execution.py
+++ execution.py
@@ -6,14 +6,8 @@ if __name__ == "__main__":
     
     web = 'https://minciencias.gov.co/convocatorias/todas'
     main_web ='https://minciencias.gov.co'
-    mailsList = ['lina.ruiz2@udea.edu.co','anderson.ruales@udea.edu.co']#,'josed.ruiz@udea.edu.co']
+    mailsList = ['lina.ruiz2@udea.edu.co','anderson.ruales@udea.edu.co']
     
     conv = mc.mincienciasConvoc(web, main_web, mailsList)# you can also set: the frequency to run and the nPages
     conv.run() 
-    #conv.get_table()
-    #conv.save()
-    #conv.comparing()
-    #conv.emailing()
-    #print(conv.table)
-    #print(conv.newones)
-    #print(conv.mails)
+    
