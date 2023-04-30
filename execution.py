0e6496ffc8f449daeb424c93c6c972fd088c15b7
linar <linar@wolfram.com>
2023-04-29 19:16:33 -0500

    adding mails

--- execution.py
+++ execution.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     
     web = 'https://minciencias.gov.co/convocatorias/todas'
     main_web ='https://minciencias.gov.co'
-    mailsList = ['lina.ruiz2@udea.edu.co','anderson.ruales@udea.edu.co']
+    mailsList = ['lina.ruiz2@udea.edu.co','anderson.ruales@udea.edu.co','paula.diaz@udea.edu.co','josed.ruiz@udea.edu.co','restrepo@udea.edu.co']
     
     conv = mc.mincienciasConvoc(web, main_web, mailsList)# you can also set: the frequency to run and the nPages
     conv.run() 
